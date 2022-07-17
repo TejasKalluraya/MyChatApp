c4acea28ee7d241d960050126c6dd9553cc7b63d
Tejas K <atejask2017@gmail.com>
2022-07-18 02:35:52 +0530

    Enabled Search in Users List Page

---
+++ php/data.php
@@ -0,0 +1,17 @@
+<?php
+        while($row = mysqli_fetch_assoc($query))
+        {
+            $output .= '
+            <a href="chat.php">
+        <div class="content" style="padding:10px">
+            <img src="php/images/'. $row['img'] .'" alt="" height=50px; width=50px>
+            <br>
+            <div class="details">
+                <span">'. $row['fname']. " " . $row['lname'] .'</span>
+                <p>test message</p>
+            <div class="status-dot"><i class="fas fa-circle"></i></div>
+        </div>
+            </div>
+            </a>';
+        }
+?>
--- php/users.php
+++ php/users.php
@@ -1,27 +1,14 @@
-<?php 
-session_start();
-include_once "config.php";
-$sql = mysqli_query($conn,"SELECT * FROM users");
-$output = "";
-if(mysqli_num_rows($sql)==1){
-    $output .= "No users are available to chat";
-}
-elseif(mysqli_num_rows($sql)>0){
-    while($row = mysqli_fetch_assoc($sql))
-    {
-        $output .= '
-        <a href="chat.php">
-    <div class="content" style="padding:10px">
-        <img src="php/images/'. $row['img'] .'" alt="" height=50px; width=50px>
-        <br>
-        <div class="details">
-            <span">'. $row['fname']. " " . $row['lname'] .'</span>
-            <p>test message</p>
-        <div class="status-dot"><i class="fas fa-circle"></i></div>
-    </div>
-        </div>
-        </a>';
+<?php
+    session_start();
+    include_once "config.php";
+    $outgoing_id = $_SESSION['unique_id'];
+    $sql = "SELECT * FROM users WHERE NOT unique_id = {$outgoing_id} ORDER BY user_id DESC";
+    $query = mysqli_query($conn, $sql);
+    $output = "";
+    if(mysqli_num_rows($query) == 0){
+        $output .= "No users are available to chat";
+    }elseif(mysqli_num_rows($query) > 0){
+        include_once "data.php";
     }
-}
-echo $output;
+    echo $output;
 ?>
